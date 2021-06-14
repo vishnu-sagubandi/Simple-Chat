7a621c1737f904f9946906cc394f454efa77c064
vishnu-sagubandi <sagubandi.vmnaidu.eee19@itbhu.ac.in>
2021-06-14 16:51:15 +0530

    Added Procfile

--- Justchat/settings.py
+++ Justchat/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-wny($jmx&%y-3msgg7h0tq9$vr@rew+$g90onodv7vv%o)!#y9
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
@@ -43,6 +43,7 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
@@ -130,6 +131,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIC_ROOT = BASE_DIR/'statifiles'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
