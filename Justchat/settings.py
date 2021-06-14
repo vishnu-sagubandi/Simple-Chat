5b60db4bc0406ded604e9ff485a7d6184face355
vishnu-sagubandi <sagubandi.vmnaidu.eee19@itbhu.ac.in>
2021-06-14 16:44:17 +0530

    Update

--- Justchat/settings.py
+++ Justchat/settings.py
@@ -72,6 +72,15 @@ TEMPLATES = [
 WSGI_APPLICATION = 'Justchat.wsgi.application'
 ASGI_APPLICATION = 'Justchat.asgi.application'
 
+CHANNEL_LAYERS = {
+    'default': {
+        'BACKEND': 'channels_redis.core.RedisChannelLayer',
+        'CONFIG': {
+            "hosts": [('127.0.0.1', 6379)],
+        },
+    },
+}
+
 
 # Database
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
