0676cfda594228d6d3b37d9e184253fbee4a25fc
vishnu-sagubandi <sagubandi.vmnaidu.eee19@itbhu.ac.in>
2021-06-15 12:37:51 +0530

    Update

--- Justchat/settings.py
+++ Justchat/settings.py
@@ -13,6 +13,8 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 from pathlib import Path
 import os
 
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "Justchat.settings")
+
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
