095546a315b6d470a3dc93f75762d39d0fca3d62
Mekon101 <sagubandi.vmnaidu.eee19@itbhu.ac.in>
2021-06-15 14:25:14 +0530

    Update asgi.py

--- Justchat/asgi.py
+++ Justchat/asgi.py
@@ -8,6 +8,8 @@ https://docs.djangoproject.com/en/3.2/howto/deployment/asgi/
 """
 import os
 import django
+from django.conf.urls import url
+from django.core.asgi import get_asgi_application
 
 # Fetch Django ASGI application early to ensure AppRegistry is populated
 # before importing consumers and AuthMiddlewareStack that may import ORM
@@ -17,8 +19,6 @@ os.environ.setdefault("DJANGO_SETTINGS_MODULE", "Justchat.settings")
 django.setup()
 django_asgi_app = get_asgi_application()
 
-from django.conf.urls import url
-from django.core.asgi import get_asgi_application
 
 import chat.routing
 from channels.routing import ProtocolTypeRouter, URLRouter
