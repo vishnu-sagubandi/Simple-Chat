4e4fccbe88b6abb0425452a96fcd035ea50b3254
vishnu-sagubandi <sagubandi.vmnaidu.eee19@itbhu.ac.in>
2021-06-15 14:16:13 +0530

    Update

--- Justchat/asgi.py
+++ Justchat/asgi.py
@@ -6,15 +6,21 @@ It exposes the ASGI callable as a module-level variable named ``application``.
 For more information on this file, see
 https://docs.djangoproject.com/en/3.2/howto/deployment/asgi/
 """
-import django
-from channels.auth import AuthMiddlewareStack
-from channels.routing import ProtocolTypeRouter, URLRouter
-from django.core.asgi import get_asgi_application
 import chat.routing
+from channels.routing import ProtocolTypeRouter, URLRouter
+from channels.auth import AuthMiddlewareStack
 import os
+import django
+
+from django.conf.urls import url
+from django.core.asgi import get_asgi_application
+
+# Fetch Django ASGI application early to ensure AppRegistry is populated
+# before importing consumers and AuthMiddlewareStack that may import ORM
+# models.
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "Justchat.settings")
 django.setup()
-
+django_asgi_app = get_asgi_application()
 
 application = ProtocolTypeRouter({
     "http": get_asgi_application(),
--- Justchat/settings.py
+++ Justchat/settings.py
@@ -9,11 +9,9 @@ https://docs.djangoproject.com/en/3.2/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/3.2/ref/settings/
 """
-
 from pathlib import Path
 import os
 
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "Justchat.settings")
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
