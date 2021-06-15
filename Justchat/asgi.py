e402b897f23f28c56ff82c8b681ca6f484e50eef
Mekon101 <sagubandi.vmnaidu.eee19@itbhu.ac.in>
2021-06-15 14:24:31 +0530

    Update asgi.py

--- Justchat/asgi.py
+++ Justchat/asgi.py
@@ -6,21 +6,25 @@ It exposes the ASGI callable as a module-level variable named ``application``.
 For more information on this file, see
 https://docs.djangoproject.com/en/3.2/howto/deployment/asgi/
 """
-import chat.routing
-from channels.routing import ProtocolTypeRouter, URLRouter
-from channels.auth import AuthMiddlewareStack
 import os
 import django
-
-from django.conf.urls import url
-from django.core.asgi import get_asgi_application
 
 # Fetch Django ASGI application early to ensure AppRegistry is populated
 # before importing consumers and AuthMiddlewareStack that may import ORM
 # models.
+
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "Justchat.settings")
 django.setup()
 django_asgi_app = get_asgi_application()
+
+from django.conf.urls import url
+from django.core.asgi import get_asgi_application
+
+import chat.routing
+from channels.routing import ProtocolTypeRouter, URLRouter
+from channels.auth import AuthMiddlewareStack
+
+
 
 application = ProtocolTypeRouter({
     "http": get_asgi_application(),
